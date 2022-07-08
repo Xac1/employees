34faaadd93ba8f75fa97e7a7481a12d245418cb7
Xac1 <xac_cs@ukr.net>
2022-07-08 18:43:04 +0000

    first commit

---
+++ import.sh
@@ -0,0 +1,8 @@
+
+#Ubuntu
+sudo tar zxfv emp.tar.gz && sudo /usr/bin/mysql -u root -p < emp.sql
+
+#MacOS
+#cd ~/Downloads/
+#git clone 
+#sudo tar zxfv emp.tar.gz && sudo /usr/local/mysql/bin/mysql -u root -p < emp.sql
